6a91279aae8feb8f4cacc38601c200cb915e2830
Katarina Poole <58176031+Katarina-Poole@users.noreply.github.com>
2024-03-22 14:16:10 +0000

    update configs

--- docs/conf.py
+++ docs/conf.py
@@ -12,19 +12,30 @@ sys.path.insert(0,os.path.abspath('..'))
 project = 'Spatial Audio Metrics'
 copyright = '2024, Katarina C. Poole'
 author = 'Katarina C. Poole'
+version = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    ]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['docs/_static']
+#html_static_path = ['docs/_static']
